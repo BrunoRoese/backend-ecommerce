2d0d1fd048e2e4bb118772cdde5464f94f6dd335
Bruno Roese <brunoroese@hotmail.com>
2022-04-06 19:38:26 -0300

    Implement a new method to getActiveBook and verify if product is found by the query inside bookService

--- src/main/java/com/brunoproject/ecommerce/book/service/BookService.java
+++ src/main/java/com/brunoproject/ecommerce/book/service/BookService.java
@@ -1,11 +1,14 @@
 package com.brunoproject.ecommerce.book.service;
 
+import com.brunoproject.ecommerce.book.exceptions.BookNotFoundException;
 import com.brunoproject.ecommerce.dao.ProductRepository;
 import com.brunoproject.ecommerce.entities.Product;
+import com.brunoproject.ecommerce.product.exceptions.ProductNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Service
@@ -15,5 +18,16 @@ public class BookService {
 
     public List<Product> getActiveBooks() {
         return productRepository.findAllActiveBooks();
+    }
+
+    public Product getActiveBookById(Long bookId) {
+        return verifyIfProductIsActiveAndBook(bookId);
+    }
+
+    private Product verifyIfProductIsActiveAndBook(Long bookId) {
+        return productRepository.findAllActiveBooks().stream()
+                .filter(product -> product.getId().equals(bookId))
+                .findAny()
+                .orElseThrow(BookNotFoundException::new);
     }
 }
