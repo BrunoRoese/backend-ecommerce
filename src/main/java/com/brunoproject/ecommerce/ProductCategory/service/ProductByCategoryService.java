6f19124c820f653631a2b64d0d1c684bf596ea18
Bruno Roese <brunoroese@hotmail.com>
2022-05-08 18:40:09 -0300

    Add service annotation to productByCategoryService

--- src/main/java/com/brunoproject/ecommerce/ProductCategory/service/ProductByCategoryService.java
+++ src/main/java/com/brunoproject/ecommerce/ProductCategory/service/ProductByCategoryService.java
@@ -1,12 +1,15 @@
 package com.brunoproject.ecommerce.ProductCategory.service;
 
 import com.brunoproject.ecommerce.Mapper.ProductMapper;
+import com.brunoproject.ecommerce.Product.exceptions.ProductNotFoundException;
 import com.brunoproject.ecommerce.ProductCategory.exceptions.ProductCategoryIsEmpty;
 import com.brunoproject.ecommerce.ProductDao.ProductRepository;
 import com.brunoproject.ecommerce.ProductEntities.ProductDto;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
 @AllArgsConstructor
 public class ProductByCategoryService {
     private final ProductRepository productRepository;
