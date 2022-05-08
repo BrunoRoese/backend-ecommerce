b8d2853eb85ad1dea915eb0c348bd9fe9e44d31d
brunobeckmann <bruno.beckmann@townsq.com.br>
2022-05-08 08:50:19 -0300

    Create a new exception for product category is empty

--- src/main/java/com/brunoproject/ecommerce/ProductCategory/exceptions/ProductCategoryIsEmpty.java
+++ src/main/java/com/brunoproject/ecommerce/ProductCategory/exceptions/ProductCategoryIsEmpty.java
@@ -1,2 +1,7 @@
-package com.brunoproject.ecommerce.ProductCategory.exceptions;public class ProductCategoryIsEmpty {
+package com.brunoproject.ecommerce.ProductCategory.exceptions;
+
+public class ProductCategoryIsEmpty extends RuntimeException{
+    public ProductCategoryIsEmpty() {
+        super("Product Category Is Empty Or Doesnt Exist");
+    }
 }
