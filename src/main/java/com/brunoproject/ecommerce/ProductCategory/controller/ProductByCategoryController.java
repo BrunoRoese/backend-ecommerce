1c109213e81f103705b194a17ac51be3e9c48537
Bruno Roese <brunoroese@hotmail.com>
2022-05-08 18:43:39 -0300

    Create a new endpoint to get product by his id

--- src/main/java/com/brunoproject/ecommerce/ProductCategory/controller/ProductByCategoryController.java
+++ src/main/java/com/brunoproject/ecommerce/ProductCategory/controller/ProductByCategoryController.java
@@ -1,8 +1,11 @@
 package com.brunoproject.ecommerce.ProductCategory.controller;
 
 import com.brunoproject.ecommerce.ProductCategory.service.ProductByCategoryService;
+import com.brunoproject.ecommerce.ProductEntities.Product;
 import com.brunoproject.ecommerce.ProductEntities.ProductDto;
+
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +22,12 @@ public class ProductByCategoryController {
     @GetMapping("/{id}")
     public List<ProductDto> listOfProductsByCategory(@PathVariable("id") Long productCategoryId) {
         return productByCategoryService.getAllProductByCategory(productCategoryId);
+    }
+
+    @GetMapping("/{productCategoryId}/{productId}")
+    public ProductDto getProductByHisId(
+            @PathVariable("productCategoryId") Long productCategoryId,
+            @PathVariable("productId") Long productId) {
+        return productByCategoryService.getSpecificProductByCategory(productId, productCategoryId);
     }
 }
